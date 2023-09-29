11580052e530922c1bd46dc9774ebf93434a7e48
TenguTerror <vegasduard@gmail.com>
2023-09-28 20:52:03 -0600

    calculator problem solved

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,19 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+  let result;
+
+  if (operator === '+') {
+    result = num1 + num2;
+  } else if (operator === '-') {
+    result = num1 - num2;
+  } else if (operator === '*') {
+    result = num1 * num2;
+  } else if (operator === '/') {
+    result = num1 / num2;
+  } else {
+    throw new Error('Invalid operator');
+  }
+
+  return result;
+}
 
 module.exports = calculator;
