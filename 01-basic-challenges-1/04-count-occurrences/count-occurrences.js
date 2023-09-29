480bacd812eb9e9cf452b1ac4ed1642fa83fa311
TenguTerror <vegasduard@gmail.com>
2023-09-28 21:13:12 -0600

    count occurence solved

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,13 @@
-function countOccurrences() {}
+function countOccurrences(str, char) {
+  let result = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === char) {
+      result++;
+    }
+  }
+
+  return result;
+}
 
 module.exports = countOccurrences;
