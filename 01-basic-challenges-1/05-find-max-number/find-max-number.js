bb40f64cdafd4291aadd394727cd38ae0c42e487
TenguTerror <vegasduard@gmail.com>
2023-09-29 07:12:32 -0600

    find-max-number soved

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,12 @@
-function findMaxNumber() {}
+function findMaxNumber(arr) {
+  let result = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > result) {
+      result = arr[i];
+    }
+  }
+  return result;
+}
 
 module.exports = findMaxNumber;
