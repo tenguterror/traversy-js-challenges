eb2d8054a6ea55f221aef3ca48884088d9f141c0
TenguTerror <vegasduard@gmail.com>
2023-09-30 19:11:26 -0600

    reverse-string with my solution and reverse loop

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,22 @@
-function reverseString() {}
+// function reverseString(str) {
+//   const reversed = [];
+
+//   for (let i = 0; i < str.length; i++) {   //  My solution using an array.
+//     reversed.unshift(str[i]);
+//   }
+
+//   return reversed.join('');
+//}
+
+function reverseString(str) {
+  let reversed = '';
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    // Way it is done with a reverse for loop
+    reversed += str[i];
+  }
+
+  return reversed;
+}
 
 module.exports = reverseString;
