d42d31976e1e8bfbecdf8083d40245d894b6e453
TenguTerror <vegasduard@gmail.com>
2023-09-30 18:46:50 -0600

    title-case solved

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,12 @@
-function titleCase() {}
+function titleCase(str) {
+  const words = str.toLowerCase().split(' ');
+  let upperCase = [];
+
+  for (let i = 0; i < words.length; i++) {
+    upperCase.push(words[i].charAt(0).toUpperCase() + words[i].slice(1));
+  }
+
+  return (upperCase = upperCase.join(' '));
+}
 
 module.exports = titleCase;
