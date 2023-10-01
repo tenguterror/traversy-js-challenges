2df337777a3dd66d3a8fb1916cc51f7b876490de
TenguTerror <vegasduard@gmail.com>
2023-10-01 13:50:55 -0600

    palindrome done with my way and traversy way

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,18 @@
-function isPalindrome() {}
+// function isPalindrome(str) {
+//   let reversed = '';
+
+//   for (let i = str.length - 1; i >= 0; i--) {  // My way of doing it
+//     // Way it is done with a reverse for loop
+//     reversed += str[i];
+//   }
+
+//   return reversed === str;
+//}
+
+function isPalindrome(str) {
+  const format = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  const reversed = format.split('').reverse().join(''); // Traversy way of doing it
+  return format === reversed;
+}
 
 module.exports = isPalindrome;
