3110eb95a65ec7aee11e7ca38696e54dbde6da5a
TenguTerror <vegasduard@gmail.com>
2023-10-03 19:30:17 -0600

    remove duplicates done

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,15 @@
-function removeDuplicates() {}
+// My way using a forEach
+
+function removeDuplicates(arr) {
+  const result = [];
+
+  arr.forEach((element) => {
+    if (!result.includes(element)) {
+      result.push(element);
+    }
+  });
+
+  return result;
+}
 
 module.exports = removeDuplicates;
