d95ff0adb4d64523fd96a18a976f4d93c8c32521
TenguTerror <vegasduard@gmail.com>
2023-10-04 07:44:11 -0600

    fizzbuzz-arr complete

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,18 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(num) {
+  const finalArr = [];
+
+  for (let i = 1; i <= num; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      finalArr.push('FizzBuzz');
+    } else if (i % 3 === 0) {
+      finalArr.push('Fizz');
+    } else if (i % 5 === 0) {
+      finalArr.push('Buzz');
+    } else {
+      finalArr.push(i);
+    }
+  }
+  return finalArr;
+}
 
 module.exports = fizzBuzzArray;
