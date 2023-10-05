5a49aa67b99a179692aa52e33564cb30fbe5cd21
TenguTerror <vegasduard@gmail.com>
2023-10-04 18:58:02 -0600

    arrayIntersection done with Set and filter method

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,9 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+  const set1 = new Set(arr1);
+  const set2 = new Set(arr2);
+
+  const commonElements = [...set1].filter((element) => set2.has(element));
+  return commonElements;
+}
 
 module.exports = arrayIntersection;
