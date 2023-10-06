aa708df599aacb16ea53da1eba4a60ee75b71e2a
TenguTerror <vegasduard@gmail.com>
2023-10-05 18:37:02 -0600

    refactoring after watching video

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -9,12 +9,8 @@ function displayLikes(name) {
     return `${name[0]} and ${name[1]} like this`;
   } else if (arrLengthName === 3) {
     return `${name[0]}, ${name[1]} and ${name[2]} like this`;
-  } else if (arrLengthName === 4) {
-    return `${name[0]}, ${name[1]} and ${arrLengthName - 2} others like this`;
-  } else if (arrLengthName === 5) {
-    return `${name[0]}, ${name[1]} and ${arrLengthName - 2} others like this`;
   } else {
-    throw new Error('Something went wrong!!');
+    return `${name[0]}, ${name[1]} and ${arrLengthName - 2} others like this`;
   }
 }
 
