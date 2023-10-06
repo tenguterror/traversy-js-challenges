a497d278a2548a6e547fcb261521c61f1a025552
TenguTerror <vegasduard@gmail.com>
2023-10-06 07:32:54 -0600

    find-missingp-number done

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,17 @@
-function findMissingNumber() {}
+function findMissingNumber(numbers) {
+  const n = numbers.length + 1;
+  const sum = (n * (n + 1)) / 2; // Gauss formula
+  let sumOfNumbers = 0;
+
+  if (!numbers || numbers.length === 0) {
+    return undefined;
+  }
+
+  for (let i = 0; i < numbers.length; i++) {
+    sumOfNumbers += numbers[i];
+  }
+
+  return sum - sumOfNumbers;
+}
 
 module.exports = findMissingNumber;
