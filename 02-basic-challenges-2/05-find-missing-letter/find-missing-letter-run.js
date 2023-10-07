e5e046cc9f772d89805a01380725de7d69e02e21
TenguTerror <vegasduard@gmail.com>
2023-10-06 19:00:22 -0600

    just a little refactor after wtching the video

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter-run.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter-run.js
@@ -1,5 +1,5 @@
 const findMissingLetter = require('./find-missing-letter');
 
-result = findMissingLetter(['a', 'b', 'c', 'd', 'f', 'g']);
+result = findMissingLetter([]);
 
 console.log(result);
--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,8 +1,4 @@
 function findMissingLetter(arr) {
-  if (arr.length === 0) {
-    return '';
-  }
-
   for (let i = 0; i < arr.length; i++) {
     let currentCharCode = arr[i].charCodeAt(0);
     let nextCharCode = arr[i + 1].charCodeAt(0);
@@ -12,7 +8,7 @@ function findMissingLetter(arr) {
     }
   }
 
-  return undefined;
+  return '';
 }
 
 module.exports = findMissingLetter;
