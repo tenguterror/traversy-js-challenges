13143560e94a543edc03325f6745c97db5838f5b
TenguTerror <vegasduard@gmail.com>
2023-10-06 18:23:39 -0600

    find-missing-lettter done had some help

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,18 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+  if (arr.length === 0) {
+    return '';
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    let currentCharCode = arr[i].charCodeAt(0);
+    let nextCharCode = arr[i + 1].charCodeAt(0);
+
+    if (nextCharCode - currentCharCode > 1) {
+      return String.fromCharCode(currentCharCode + 1);
+    }
+  }
+
+  return undefined;
+}
 
 module.exports = findMissingLetter;
