89b713ebf54ea6028b9b5204a24b3004dc2f7d2d
TenguTerror <vegasduard@gmail.com>
2023-10-08 08:01:46 -0600

    are-all-characters-unique done

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,15 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+  const set1 = new Set();
+
+  for (let i = 0; i < str.length; i++) {
+    if (!set1.has(str[i])) {
+      set1.add(str[i]);
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 module.exports = areAllCharactersUnique;
