4e2b760dbc8809ab25cf071f834f251baff1ead6
TenguTerror <vegasduard@gmail.com>
2023-10-09 19:37:14 -0600

    my first attempted and could not get to work

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,17 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(str) {
+  const uniqueMap = new Map();
+
+  uniqueMap.set(str[0], true);
+  console.log(uniqueMap);
+
+  for (let i = 1; i < str.length; i++) {
+    if (uniqueMap.has(str[i]) == false) {
+      return str[i];
+    } else {
+      uniqueMap.set(str[i], true);
+    }
+  }
+  return null;
+}
 
 module.exports = findFirstNonRepeatingCharacter;
