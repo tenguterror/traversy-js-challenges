ed8d9b0ece579dd4acea523b95d32bd6fb37a9ca
TenguTerror <vegasduard@gmail.com>
2023-10-10 07:49:19 -0600

    dice-game done with help from video

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,29 @@
-function diceGameSimulation() {}
+function diceRoll() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+function diceGameSimulation(numOfRolls) {
+  const results = [];
+
+  for (let i = 0; i < numOfRolls; i++) {
+    const dice1 = diceRoll();
+    const dice2 = diceRoll();
+    const sum = dice1 + dice2;
+
+    let result = '';
+
+    if (sum === 7 || sum === 11) {
+      result = 'win';
+    } else if (sum === 2 || sum === 3 || sum === 12) {
+      result = 'lose';
+    } else {
+      result = 'roll again';
+    }
+
+    results.push({ dice1, dice2, sum, result });
+  }
+
+  return results;
+}
 
 module.exports = diceGameSimulation;
