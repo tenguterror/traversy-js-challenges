1481ae63aca06cbc3a6c430ea381a260ad1ebd39
TenguTerror <vegasduard@gmail.com>
2023-10-09 19:37:50 -0600

    followed along the video to better understand

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,17 +1,16 @@
 function findFirstNonRepeatingCharacter(str) {
-  const uniqueMap = new Map();
+  const charCount = new Map();
 
-  uniqueMap.set(str[0], true);
-  console.log(uniqueMap);
+  for (const char of str) {
+    charCount.set(char, (charCount.get(char) || 0) + 1);
+  }
 
-  for (let i = 1; i < str.length; i++) {
-    if (uniqueMap.has(str[i]) == false) {
-      return str[i];
-    } else {
-      uniqueMap.set(str[i], true);
+  for (const char of str) {
+    if (charCount.get(char) === 1) {
+      return char;
     }
   }
+
   return null;
-}
 
 module.exports = findFirstNonRepeatingCharacter;
