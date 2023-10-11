31e59adc13f8651a036a906f0924f6728b82de3a
TenguTerror <vegasduard@gmail.com>
2023-10-11 07:03:56 -0600

    little house keeping

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -3,8 +3,6 @@ function formatPhoneNumber(number) {
   const prefix = number.slice(3, 6).join('');
   const line = number.slice(6).join('');
 
-  // console.log(area);
-
   return `(${area}) ${prefix}-${line}`;
 }
 
