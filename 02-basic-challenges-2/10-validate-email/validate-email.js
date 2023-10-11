69ea07a0c9b4c9b0b43b737e581563ac81cb90b0
TenguTerror <vegasduard@gmail.com>
2023-10-11 07:40:58 -0600

    validate-email done with reg expression

--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,3 +1,6 @@
-function validateEmail() {}
+function validateEmail(str) {
+  const regex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+  return regex.test(str);
+}
 
 module.exports = validateEmail;
