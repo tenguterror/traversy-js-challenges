7dd2819734582556814fcdff0900ccc7f5a4c547
TenguTerror <vegasduard@gmail.com>
2023-10-11 07:03:32 -0600

    format-phone-number done

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,11 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(number) {
+  const area = number.slice(0, 3).join('');
+  const prefix = number.slice(3, 6).join('');
+  const line = number.slice(6).join('');
+
+  // console.log(area);
+
+  return `(${area}) ${prefix}-${line}`;
+}
 
 module.exports = formatPhoneNumber;
