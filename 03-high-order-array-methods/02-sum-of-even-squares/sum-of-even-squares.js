dffe623bc6b71efb9a4154673d70e2359998c694
TenguTerror <vegasduard@gmail.com>
2023-10-12 07:26:15 -0600

    sum-of-even square is done

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,20 @@
-function sumOfEvenSquares() {}
+// First set is with using every method into a new variable.
+
+// Second is using one variable and chaining methods to it.
+
+function sumOfEvenSquares(arr) {
+  // const filteredArr = arr.filter((num) => num % 2 === 0);
+
+  // const squaredArr = filteredArr.map((num) => num ** 2);
+
+  // const sumOfArr = squaredArr.reduce((total, num) => total + num, 0);
+
+  const sumOfArr = arr
+    .filter((num) => num % 2 === 0)
+    .map((num) => num ** 2)
+    .reduce((total, num) => total + num, 0);
+
+  return sumOfArr;
+}
 
 module.exports = sumOfEvenSquares;
