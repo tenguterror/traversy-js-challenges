2d447a07342c6d668f903f8c909f48f8810af23a
TenguTerror <vegasduard@gmail.com>
2023-10-13 08:42:05 -0600

    calcultae-total-sales done

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,13 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(arr, tax) {
+  const convertedTax = tax * 0.01 + 1;
+
+  const totalOfItems = arr
+    .map((element) => {
+      return element.price * element.quantity;
+    })
+    .reduce((total, num) => total + num, 0);
+
+  return totalOfItems * convertedTax;
+}
 
 module.exports = calculateTotalSalesWithTax;
