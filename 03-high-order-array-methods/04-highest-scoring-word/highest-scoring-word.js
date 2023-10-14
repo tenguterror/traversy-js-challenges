31f400679b3181daf00e95491c23c23df8970f5a
TenguTerror <vegasduard@gmail.com>
2023-10-14 12:45:39 -0600

    highest=scoring-word done with help from vid

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,14 @@
-function highestScoringWord() {}
+function highestScoringWord(str) {
+  const strArr = str.split(' ');
+
+  const wordscoreCal = strArr.map((e) =>
+    Array.from(e).reduce((total, num) => total + num.charCodeAt(0) - 96, 0)
+  );
+
+  const highestScore = Math.max(...wordscoreCal);
+  const highIndex = wordscoreCal.indexOf(highestScore);
+
+  return strArr[highIndex];
+}
 
 module.exports = highestScoringWord;
