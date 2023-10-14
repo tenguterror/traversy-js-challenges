5755c3392cfadf20eaa35c282dfb0510ba05dc26
TenguTerror <vegasduard@gmail.com>
2023-10-14 14:28:20 -0600

    valid-anagram doen with help from chatgpt and vid

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,17 @@
-function validAnagrams() {}
+function validAnagrams(str1, str2) {
+  const words1 = str1.split('').reduce((acc, char) => {
+    acc[char] = (acc[char] || 0) + 1;
+    return acc;
+  }, {});
+
+  const words2 = str2.split('').reduce((acc, char) => {
+    acc[char] = (acc[char] || 0) + 1;
+    return acc;
+  }, {});
+  console.log(words1);
+  console.log(words2);
+
+  return Object.keys(words1).every((char) => words1[char] === words2[char]);
+}
 
 module.exports = validAnagrams;
