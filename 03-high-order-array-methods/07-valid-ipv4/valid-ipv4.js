0fa3b0e92e5bf3035e4dbe0858918dbc58beeec8
TenguTerror <vegasduard@gmail.com>
2023-10-15 11:39:27 -0600

    started the challenge

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,7 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (str) => {
+  if (str.split('.').length < 3 || str.split('.').length > 4) {
+    return false;
+  }
+};
 
 module.exports = isValidIPv4;
