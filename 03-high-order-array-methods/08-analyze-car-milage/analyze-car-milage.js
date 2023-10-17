823091c3ed51034bba17cc6be83fcec8bec219e0
TenguTerror <vegasduard@gmail.com>
2023-10-17 07:33:07 -0600

    analyze-car-mileage help with solution on reduce

--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,3 +1,21 @@
-function analyzeCarMileage() {}
+function analyzeCarMileage(arr) {
+  const totalMileage = arr.reduce((total, miles) => total + miles.mileage, 0);
+  const averageMileage = totalMileage / arr.length;
+  const highestMileage = arr.reduce(
+    (highest, car) => (car.mileage > highest.mileage ? car : highest),
+    arr[0]
+  );
+  const lowestMileage = arr.reduce(
+    (lowest, car) => (car.mileage < lowest.mileage ? car : lowest),
+    arr[0]
+  );
+
+  return {
+    averageMileage: parseFloat(averageMileage.toFixed(2)),
+    highestMileage,
+    lowestMileage,
+    totalMileage,
+  };
+}
 
 module.exports = analyzeCarMileage;
