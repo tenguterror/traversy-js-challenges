e42be5b0358f5606f41b85b136c0b90698228742
TenguTerror <vegasduard@gmail.com>
2023-10-20 06:27:32 -0600

    just add the recuresion to make it work

--- 04-recursion/01-recursion-intro/count-down.js
+++ 04-recursion/01-recursion-intro/count-down.js
@@ -1,3 +1,14 @@
-function countDown() {}
+function countDown(num) {
+  // Base case
+  if (num <= 0) {
+    console.log('All done!');
+    return;
+  }
+
+  // Recursive case
+  console.log(num);
+  // num--;
+  countDown(num - 1);
+}
 
 module.exports = countDown;
