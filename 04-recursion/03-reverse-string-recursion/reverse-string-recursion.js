aae01bc6e115d49f113fe86b1fd94d50dd048b11
TenguTerror <vegasduard@gmail.com>
2023-10-21 20:36:14 -0600

    first help with soloution. Second help online

--- 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
+++ 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
@@ -1,3 +1,28 @@
-function reverseString() {}
+// function reverseString(str) {
+//   if (str === '') {
+//     return '';
+//   }
+
+//   return reverseString(str.substring(1)) + str[0];
+// }
+
+function reverseString(inputString) {
+  if (inputString.length <= 1) {
+    return inputString;
+  }
+
+  // Recursive step: Swap the first and last characters and reverse the substring in between.
+  const firstChar = inputString[0];
+  const lastChar = inputString[inputString.length - 1];
+  const middle = inputString.slice(1, -1);
+
+  // Recursively reverse the middle part of the string.
+  const reversedMiddle = reverseString(middle);
+
+  // Concatenate the last character, the reversed middle, and the first character.
+  const reversedString = lastChar + reversedMiddle + firstChar;
+
+  return reversedString;
+}
 
 module.exports = reverseString;
