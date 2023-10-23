3c87bfbba6b64b85755d355adabb1b72d10fa51d
TenguTerror <vegasduard@gmail.com>
2023-10-23 07:30:45 -0600

    fibonacci done with small help from video

--- 04-recursion/04-fibonacci-sequence/fibonacci-run.js
+++ 04-recursion/04-fibonacci-sequence/fibonacci-run.js
@@ -1,5 +1,5 @@
 const fibonacci = require('./fibonacci');
 
-const result = fibonacci(8);
+const result = fibonacci(55);
 
 console.log(result);
--- 04-recursion/04-fibonacci-sequence/fibonacci.js
+++ 04-recursion/04-fibonacci-sequence/fibonacci.js
@@ -1,3 +1,9 @@
-function fibonacci() {}
+function fibonacci(num) {
+  if (num < 2) {
+    return num;
+  }
+
+  return fibonacci(num - 1) + fibonacci(num - 2);
+}
 
 module.exports = fibonacci;
