5aac2a21fbcda84a4a32b5f73e6604e3a1e06dd9
TenguTerror <vegasduard@gmail.com>
2023-10-24 07:52:19 -0600

    power done, little help with recursive statement

--- 04-recursion/06-power/power.js
+++ 04-recursion/06-power/power.js
@@ -1,3 +1,12 @@
-function power() {}
+function power(num, exp) {
+  // console.log(exp);
+  console.log(num);
+
+  if (exp === 0) {
+    return 1;
+  }
+
+  return num * power(num, exp - 1);
+}
 
 module.exports = power;
