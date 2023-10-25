94709b7fe45e90785b217b4b218fd6a8eecfc981
TenguTerror <vegasduard@gmail.com>
2023-10-25 07:57:11 -0600

    array-sum done. Used shift to solve the problem

--- 04-recursion/07-array-sum/array-sum.js
+++ 04-recursion/07-array-sum/array-sum.js
@@ -1,3 +1,12 @@
-function arraySum() {}
+function arraySum(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+
+  const element = arr.shift();
+  console.log(element);
+
+  return arraySum(arr) + element;
+}
 
 module.exports = arraySum;
