49689e5c288ea6568eea0f2f5e9061460b6b5837
TenguTerror <vegasduard@gmail.com>
2023-10-26 07:34:01 -0600

    number-range doen with some help

--- 04-recursion/08-number-range/number-range-run.js
+++ 04-recursion/08-number-range/number-range-run.js
@@ -1,5 +1,5 @@
 const numberRange = require('./number-range');
 
-const result = numberRange(1, 5);
+const result = numberRange(3, 10);
 
 console.log(result);
--- 04-recursion/08-number-range/number-range.js
+++ 04-recursion/08-number-range/number-range.js
@@ -1,3 +1,11 @@
-function numberRange() {}
+function numberRange(startNum, endNum) {
+  if (endNum === startNum) {
+    return [startNum];
+  }
+
+  let result = numberRange(startNum, endNum - 1);
+  result.push(endNum);
+  return result;
+}
 
 module.exports = numberRange;
